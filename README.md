cf8f0880a498193fa73d140fc85f16aff32a3258
gustavoOlmos <programacionwebolmos@gmail.com>
2025-09-17 18:25:07 -0300

    Se modifican los archivos README y Main

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Soy Gustavo Olmos y estoy cursando la ***Tecnicatura de Desarrollo de Software*** en el ISSD 📚📒📗📖
 
-* Completé el achivo [Main](https://github.com/Gabriel-ISSD/clase-2-Gusta2025-droid/blob/main/src/main/java/org/example/Main.java) con lo visto en clase ya que estaba sin modificar.
+* Completé el achivo [Main](https://github.com/Gabriel-ISSD/clase-2-Gusta2025-droid/blob/main/src/main/java/org/example/Main.java) con lo visto en clase ya que estaba sin modificar 🤗.
 * Dentro de este ClassRoom agregué una carpeta de nombre [practice](https://github.com/Gabriel-ISSD/clase-2-Gusta2025-droid/tree/main/src/practice/java)
 con la resolución de los [Proyectos de Ejemplo del pdf de la clase 2](https://github.com/Gabriel-ISSD/clase-2-Gusta2025-droid/tree/main/src/practice/java/EjemplosEnClases) 
 y otra con los [Problemas Propuestos Resueltos](https://github.com/Gabriel-ISSD/clase-2-Gusta2025-droid/tree/main/src/practice/java/ProblemasPropuestos)!
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,49 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Ciclo While:
+
+        System.out.printf("while loop: \n");//es el equivalente más cercano a los f-strings de Python en Java. Usamos "," para separar en lugar del "+"
+        int i = 0;//Contador usado para controlar la cantidad de ciclos, sino se hace un ciclo infinito.
+        while (i < 5) {
+            System.out.println("i: "+ i);
+            i++;
+        }
+
+        //Ciclo Do While: reproduce aunque sea una vez.
+        System.out.printf("do while loop: \n");
+        int j = 0;
+        do{
+            System.out.println("j: "+ j);
+            j++;
+        }while(j<3);
+
+        //Ciclo for:
+        System.out.printf("for loop: \n");
+        for (int k=0; k<3; k++){
+            System.out.println("k: "+ k);
+        }
+
+        //++ j=j+1
+        //-- j=j-1
+        //+= j=j+2
+        //-= j=j-2
+        //*= j=j*2
+        // /= j=j/2
+        // %= j=j%2
+
+        //Ciclo for each: recorre elementos de una lista.
+        System.out.printf("for each loop: \n");
+        String[] list_name={"Ana", "Luis", "Pedro"};
+        for(String name: list_name){
+            System.out.println("name: "+ name);
+        }
+
+        String palabra="Hola mundo";
+        for (char letra : palabra.toCharArray()){//toCharArray(): crea una lista con todos los caracteres del string.
+            System.out.println("letra: "+ letra);//Imprime letra por letra.
         }
     }
 }
